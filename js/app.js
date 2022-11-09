e7ff37ee4d47b9f440e3c5796464ffdc57cd4c36
VictorLascares <vmlascares@gmail.com>
2022-11-09 12:21:59 -0600

    Seleccionando los elementos de la interfaz

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleccionar los elementos de la interfaz
+  const formulario = document.querySelector("#formulario");
+  const entradaCorreo = document.querySelector("#email");
+  const entradaAsunto = document.querySelector("#asunto");
+  const entradaMensaje = document.querySelector("#mensaje");
+});
