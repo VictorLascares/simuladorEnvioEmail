904040efba7dabf55054f7eafafa22a1221277a6
VictorLascares <vmlascares@gmail.com>
2022-11-09 12:33:36 -0600

    Creando una funcion para validar las entradas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Seleccionar los elementos de la interfaz
-  const formulario = document.querySelector("#formulario");
   const entradaCorreo = document.querySelector("#email");
   const entradaAsunto = document.querySelector("#asunto");
   const entradaMensaje = document.querySelector("#mensaje");
+
+  // Asignar eventos
+  entradaCorreo.addEventListener("blur", validar);
+  entradaAsunto.addEventListener("blur", validar);
+  entradaMensaje.addEventListener("blur", validar);
+
+  function validar(e) {
+    console.log(e.target.value);
+  }
 });
